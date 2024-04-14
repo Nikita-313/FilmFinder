5b8c829362d16864328d1b734ca55fac2814c2ac
Nikita <vorobeu224@mail.ru>
2024-04-14 16:18:34 +0300

    Network module code refactor

--- app/src/main/java/com/cinetech/filmfinder/di/data_module/NetworkModule.kt
+++ app/src/main/java/com/cinetech/filmfinder/di/data_module/NetworkModule.kt
@@ -11,14 +11,20 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 @Module
 class NetworkModule {
+    companion object{
+        private const val API_KEY_NAME = "X-API-KEY"
+        //Write token here:
+        private const val API_KEY = ""
+        private const val API_BASE_URL = "https://api.kinopoisk.dev"
+    }
 
     @Provides
     fun providesOkHttpClient(): OkHttpClient {
         return OkHttpClient().newBuilder().addInterceptor { chain ->
             val originalRequest: Request = chain.request()
             val builder: Request.Builder = originalRequest.newBuilder().header(
-                "X-API-KEY",
-                ""
+                API_KEY_NAME,
+                API_KEY
             )
             val newRequest: Request = builder.build()
             chain.proceed(newRequest)
@@ -31,7 +37,7 @@ class NetworkModule {
     ): MovieService {
         return Retrofit.Builder()
             .client(client)
-            .baseUrl("https://api.kinopoisk.dev")
+            .baseUrl(API_BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(MovieService::class.java)
